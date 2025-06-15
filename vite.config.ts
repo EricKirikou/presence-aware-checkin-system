224427a7f14581ba99c4c2bfb5312de01670cf7d
Eric Owiredu Akumia Lartey <erickirikou123@gmail.com>
2025-06-15 20:15:20 +0000

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       external: [],
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
